69e07bda09ee4c15ae09e25c235ad8b0585d7fe9
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-17 13:03:53 +0900

    문제 Ex09 (숫자 증가하며 출력하기)

---
+++ src/com/javaex/practice03/Ex09.java
@@ -0,0 +1,20 @@
+package com.javaex.practice03;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		int i,j;
+		int u;
+		
+		for(i=0;i<10;i++){
+			for(j=1;j<=10;j++) {
+				
+				u = i +j;
+				
+				System.out.print(u+"\t");
+			}System.out.print("\n");
+		}
+
+	}
+
+}
