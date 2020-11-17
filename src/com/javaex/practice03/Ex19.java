1cca9544366fffcfd8a2245f7c85fb9f4d7a4e44
'Ryu-Yena' <'uyti0713@gmail.com'>
2020-11-17 16:00:19 +0900

    문제 Ex19 (은행 프로그램 만들기)

---
+++ src/com/javaex/practice03/Ex19.java
@@ -0,0 +1,41 @@
+package com.javaex.practice03;
+
+import java.util.Scanner;
+
+public class Ex19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num;
+		int money=0, income,outcome; 
+		
+		while(true) {
+			System.out.println("---------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("---------------------------------");
+			System.out.print("선택>");
+			num = sc.nextInt();
+			
+			if(num==1) {
+				System.out.print("예금액>");
+				income = sc.nextInt();
+				
+				money = income + money;
+			}else if(num==2) {
+				System.out.print("출금액>");
+				outcome = sc.nextInt();
+				
+				money = money - outcome;
+			}else if(num==3) {
+				System.out.println("잔고액>"+money);
+			}else if(num==4) {
+				System.out.println("프로그램 종료");
+				break;
+			}else System.out.println("다시입력해주세요.");
+		}
+		sc.close();
+
+	}
+
+}
